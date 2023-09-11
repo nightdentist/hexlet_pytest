c10772924978d62573b9d4232621b555d8521fd3
Alexander Kolenchenko <sasha_2014@mail.ru>
2023-09-11 11:55:56 +0300

    add tests1

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,11 +1,10 @@
 import pytest
 
-from hexlet_pytest.stack import stack
-
 
 def test_pop_with_empty_stack():
     stack = []
-    # проверить что вызывается конкретное исключение можно с помощью конструкции with pytest.raises()
+    # проверить что вызывается конкретное исключение
+    # можно с помощью конструкции with pytest.raises()
     # если внутри блока вызовется исключение, то тест будет пройден
     with pytest.raises(IndexError):
         stack.pop()
